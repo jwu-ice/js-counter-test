0bf928aa9a55ff3b38d5f07a1988d2f25b71e975
jwu-ice <sju02048@naver.com>
2022-09-01 20:30:17 +0900

    cypress-test-counter

---
+++ cypress/e2e/2-advanced-examples/counter.cy.js
@@ -0,0 +1,63 @@
+// - [ ] counter의 초기값은 0이다.
+// - [ ] + 버튼을 클릭 시 count가 1증가한다.
+// - [ ] - 버튼을 클릭 시 count가 1감소한다.
+// - [ ] + 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)
+// - [ ] - 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)
+// - [ ] reset 버튼을 클릭 시 counter가 0으로 초기화된다.
+
+describe("example counter app", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5500/");
+  });
+
+  it("counter의 초기값은 0이다.", () => {
+    cy.get("#value").invoke("text").should("eq", "0");
+  });
+
+  it("+ 버튼을 클릭 시 count가 1증가한다.", () => {
+    cy.get("#value")
+      .invoke("text")
+      .then((value) => {
+        const preValue = Number(value);
+        cy.get(".increase-btn").click();
+        cy.get("#value")
+          .invoke("text")
+          .should("eq", String(preValue + 1));
+      });
+  });
+
+  it("- 버튼을 클릭 시 count가 1감소한다.", () => {
+    cy.get(".increase-btn").click();
+    cy.get("#value")
+      .invoke("text")
+      .then((value) => {
+        const preValue = Number(value);
+        cy.get(".decrease-btn").click();
+        cy.get("#value")
+          .invoke("text")
+          .should("eq", String(preValue - 1));
+      });
+  });
+
+  it("+ 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)", () => {
+    for (let i = 0; i < 20; i++) {
+      cy.get(".increase-btn").click();
+    }
+
+    cy.get("#value").invoke("text").should("eq", "10");
+  });
+
+  it("- 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)", () => {
+    for (let i = 0; i < 5; i++) {
+      cy.get(".decrease-btn").click();
+    }
+
+    cy.get("#value").invoke("text").should("eq", "0");
+  });
+
+  it("reset 버튼을 클릭 시 counter가 0으로 초기화된다.", () => {
+    cy.get(".increase-btn").click();
+    cy.get(".reset-btn").click();
+    cy.get("#value").invoke("text").should("eq", "0");
+  });
+});
